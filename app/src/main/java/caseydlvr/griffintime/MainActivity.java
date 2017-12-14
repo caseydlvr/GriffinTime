ab68c09994af2bfc47e2adbd51c362f996c29f0a
Casey de la Vega-Rawson <spamcasey@gmail.com>
2017-12-14 01:43:49 -0800

    remove unused import

--- app/src/main/java/caseydlvr/griffintime/MainActivity.java
+++ app/src/main/java/caseydlvr/griffintime/MainActivity.java
@@ -8,7 +8,6 @@ import android.content.pm.ActivityInfo;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
